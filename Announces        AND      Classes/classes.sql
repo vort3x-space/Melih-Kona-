4b02bd55a15cbc2907a9f0aedb05782b510a4987
vort3x-space <56139785+vort3x-space@users.noreply.github.com>
2019-10-10 01:31:05 +0300

    Add files via upload

---
+++ Announces        AND      Classes/classes.sql
@@ -0,0 +1,48 @@
+CREATE TABLE classes(
+    class INT,
+    class_student_count INT,
+    class_course_count INT,
+    classes_sum_credit INT
+); 
+
+INSERT INTO classes
+(
+    class,
+    class_student_count,
+    class_course_count,
+    classes_sum_credit
+)
+VALUES(
+ 2, 
+ 10,
+ 1,
+ 8
+ );
+
+INSERT INTO classes
+(
+    class,
+    class_student_count,
+    class_course_count,
+    classes_sum_credit
+)
+VALUES(
+ 3,
+ 9,
+ 3,
+ 7
+);
+
+INSERT INTO classes
+(
+    class,
+    class_student_count,
+    class_course_count,
+    classes_sum_credit
+)
+VALUES(
+ 1, 
+ 5, 
+ 4, 
+ 10
+);
